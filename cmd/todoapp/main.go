95c766956f73291e1fcf6d6e9b43b10ce5170eec
lfssxx <grydzyn2015@gmail.com>
2026-04-19 05:54:02 +0200

    tasks stats feture was added

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -13,6 +13,9 @@ import (
 	core_pgx_pool "github.com/lfssxxx/Golang-Todoapp/internal/core/repository/postgres/pool/pgx"
 	core_http_middleware "github.com/lfssxxx/Golang-Todoapp/internal/core/transport/http/middleware"
 	core_http_server "github.com/lfssxxx/Golang-Todoapp/internal/core/transport/http/server"
+	statistics_postgres_repository "github.com/lfssxxx/Golang-Todoapp/internal/features/statistics/repository/postgres"
+	statistics_service "github.com/lfssxxx/Golang-Todoapp/internal/features/statistics/service"
+	statistics_transport_http "github.com/lfssxxx/Golang-Todoapp/internal/features/statistics/transport/http"
 	tasks_postgres_repository "github.com/lfssxxx/Golang-Todoapp/internal/features/tasks/repository/postgres"
 	tasks_service "github.com/lfssxxx/Golang-Todoapp/internal/features/tasks/service"
 	tasks_transport_http "github.com/lfssxxx/Golang-Todoapp/internal/features/tasks/transport/http"
@@ -62,6 +65,11 @@ func main() {
 	tasksService := tasks_service.NewTasksService(tasksRepository)
 	tasksTransportHTTP := tasks_transport_http.NewTasksHTTPHandler(tasksService)
 
+	logger.Debug("initializing feature", zap.String("feature", "statistics"))
+	statisticsRepository := statistics_postgres_repository.NewStatisticsRepository(pool)
+	statisticsService := statistics_service.NewStatisticsService(statisticsRepository)
+	statisticsTransportHTTP := statistics_transport_http.NewStatisticsHTTPHandler(statisticsService)
+
 	logger.Debug("initializing HTTP Server")
 
 	httpServer := core_http_server.NewHTTPServer(
@@ -75,6 +83,7 @@ func main() {
 	apiVersionRouterV1 := core_http_server.NewAPIVersionRouter(core_http_server.ApiVersion1)
 	apiVersionRouterV1.RegisterRoutes(usersTransportHTTP.Routes()...)
 	apiVersionRouterV1.RegisterRoutes(tasksTransportHTTP.Routes()...)
+	apiVersionRouterV1.RegisterRoutes(statisticsTransportHTTP.Routes()...)
 
 	/*
 		Example of usage apiVersionRouterV2 with separate Middlewares
